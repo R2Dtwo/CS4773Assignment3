64bd28689cb2ae4c93de022d70d42f08b465bd80
alifyamusa <alifyamusa@172.24.21.107>
2017-11-10 22:51:31 -0600

    Made Builder & cleaner code

--- src/planet/delegators/Planet.java
+++ src/planet/delegators/Planet.java
@@ -1,14 +1,17 @@
 package planet.delegators;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 
-public class Planet
-{
+public class Planet {
+	/// this will need to be restructured to follow robinson's clean coding rules
 	private String planetImagePath;
 	private Image planetImage;
 	private String planetName;
@@ -18,9 +21,8 @@ public class Planet
 	private double planetMeanSurfaceTempF;
 	private int planetNumberOfMoons;
 	
-	public Planet ( )
-	{
-		planetImagePath = "";
+	public Planet ( ) {
+ 		planetImagePath = "";
 		planetName = "";
 		planetDiameterKM = -1; // getter should return empty string if value invalid
 		planetDiameterM = -1;
@@ -29,37 +31,151 @@ public class Planet
 		planetNumberOfMoons = -1;
 	}
 	
-	// delegate??
-    public void setPlanetImage( String imagePath )
-    {
-    		try
-    		{
-	    		Image image = new Image(new FileInputStream(imagePath));
-	    		planetImage = image;
-	    		planetImagePath = imagePath;
+    public void setPlanetImage( String imagePath ) {
+    		try {
+    			setImagePath(imagePath);
 	    	}
-	    	catch (FileNotFoundException e)
-	    	{
+	    	catch (FileNotFoundException e) {
 	    		showErrorAlert("Error: invalid file\n" + e.getLocalizedMessage());
 	    	}
     }
     
-    public Image getPlanetImage( )
-    {
-    	return planetImage;
+    private void setImagePath( String imagePath ) throws FileNotFoundException {
+    		Image image = new Image(new FileInputStream(imagePath));
+		planetImage = image;
+		planetImagePath = imagePath;
     }
     
-    public String getPlanetImagePath( )
-    {
-    	return planetImagePath;
+    public Image getPlanetImage( ) {
+    		return planetImage;
+    }
+    
+    public String getPlanetImagePath( ) {
+    		return planetImagePath;
+    }
+     
+    public void savePlanetName(String name) {
+	    	if (name.length() < 0 || name.length() > 256)
+	    		this.showErrorAlert("Error: Planet name must be between 1 and 256 characters long");
+	    	else if (name.matches("^[A-Za-z0-9 \\-\\.]+$")) {
+	    		planetName = name;
+	    		return;
+	    	}
+	    	else
+	    		this.showErrorAlert("Error: Planet name contains invalid characters [A-Za-z0-9 -.]");
+    }
+    
+    public String getPlanetName() {
+    		return planetName;
     }
 
-    public void showErrorAlert( String errorMessage )
-    {
-    	Alert alert = new Alert(AlertType.ERROR);
-    	alert.setTitle("Invalid Planet Input");
-    	alert.setContentText(errorMessage);
-    	System.err.println(errorMessage);
-    	alert.showAndWait();
+    public void savePlanetDiameter( String planetDiameterKMText ) {
+	    	try	{
+	    		checkDiameterToSet(planetDiameterKMText);
+	    	}
+	    	catch (NumberFormatException e) {
+	    		this.showErrorAlert("Error: Invalid diameter (KM) input\n" + e.getLocalizedMessage());
+	    	}
     }
+    
+    private void checkDiameterToSet( String planetDiameterKMText ) {
+    		double diameterKM = Double.parseDouble(planetDiameterKMText);
+
+    		if (diameterKM < 0 || diameterKM > 200000)
+			throw new NumberFormatException("Diameter (KM) must be between 0 and 200,000");
+		
+		planetDiameterKM = diameterKM;
+		planetDiameterM = diameterKM * 0.621371;
+    }
+    
+    public String getPlanetDiameterM()
+    {
+    		if (planetDiameterM == -1)
+    			return "";
+    		
+    		return String.format("%,.1f", planetDiameterM);
+    }
+    
+    public String getPlanetDiameterKM()
+    {
+    		if (planetDiameterKM == -1)
+    			return "";
+    		
+    		return String.format("%,.1f", planetDiameterKM);
+
+    }
+    
+    public void savePlanetMeanSurfaceTemp( String planetSurfaceTempCText ) {
+	    	try 	{
+	    		checkTempToSet(planetSurfaceTempCText);
+	    	}
+	    	catch (NumberFormatException e) {
+	    		this.showErrorAlert("Error: Invalid temperature (C) input\n" + e.getLocalizedMessage());
+	    	}
+    }
+    
+    private void checkTempToSet( String planetSurfaceTempCText ) {
+		double meanSurfaceTempC = Double.parseDouble(planetSurfaceTempCText);
+		
+		if (meanSurfaceTempC < -273.15 || meanSurfaceTempC > 500.0)
+			throw new NumberFormatException("Degrees (C) must be between -273.15 and 500.00");
+		
+		planetMeanSurfaceTempC = meanSurfaceTempC;
+		planetMeanSurfaceTempF = ((meanSurfaceTempC * 9 / 5) + 32);
+    }
+    
+    public String getPlanetMeanSurfaceTempC() {
+    		if (planetMeanSurfaceTempC == -1)
+    			return "";
+    		return (String) (""+planetMeanSurfaceTempC);
+    }
+    
+    public String getPlanetMeanSurfaceTempF() {
+    		if (planetMeanSurfaceTempF == -1)
+    			return "";
+    		return (String) (""+planetMeanSurfaceTempF);
+    }
+    
+    public void savePlanetNumberOfMoons( String numberOfMoonsText ) {
+    		try {
+    			checkMoonsToSet(numberOfMoonsText);
+	    	}
+	    	catch (NumberFormatException e) {
+	    		this.showErrorAlert("Error: Invalid Integer input\n" + e.getLocalizedMessage());
+	    	}
+    }
+    
+    private void checkMoonsToSet( String numberOfMoonsText ) {
+		int numberOfMoons = Integer.parseInt(numberOfMoonsText);
+		
+		if (numberOfMoons < 0 || numberOfMoons > 1000)
+			throw new NumberFormatException("Number of moons must be between 0 and 1,000");
+		
+		planetNumberOfMoons = numberOfMoons;
+    }
+    
+    public String getPlanetNumberOfMoons() {
+    		if (planetNumberOfMoons == -1)
+    			return "";
+    		return String.format("%,d", planetNumberOfMoons);
+    }
+    
+    public void showErrorAlert( String errorMessage ) {
+	    	Alert alert = new Alert(AlertType.ERROR);
+	    	alert.setTitle("Invalid Planet Input");
+	    	alert.setContentText(errorMessage);
+	    	System.err.println(errorMessage);
+	    	alert.showAndWait();
+    }
+/*
+	public String getPlanetInformationString() {
+	    	String planetInformation = "";    	
+	    	planetInformation += planetImage + "\n";
+	    	planetInformation += planetName + "\n";
+	    	planetInformation += planetDiameterKM + "\n";
+	    	planetInformation += planetMeanSurfaceTempC + "\n";
+	    	planetInformation += planetNumberOfMoons + "\n";
+	    	
+	    	return planetInformation;
+	}*/
 }
--- src/planet/detail/PlanetController.java
+++ src/planet/detail/PlanetController.java
@@ -25,322 +25,144 @@ import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
+import planet.builders.PlanetBuilder;
 import planet.delegators.Planet;
 
-public class PlanetController implements Initializable
-{
-	//Alif(a) was here
-	//Poot was here too
-    @FXML
-    private ImageView planetImage;
-
-    @FXML
-    private Button selectImageButton;
-
-    @FXML
-    private TextField planetName;
-
-    @FXML
-    private TextField planetDiameterKM;
-
-    @FXML
-    private TextField planetDiameterM;
-
-    @FXML
-    private TextField planetMeanSurfaceTempC;
-
-    @FXML
-    private TextField planetMeanSurfaceTempF;
-
-    @FXML
-    private TextField planetNumberOfMoons;
-
-    @FXML
-    private Label fancyPlanetName;
+public class PlanetController implements Initializable {
+    @FXML private ImageView planetImage;
+    
+    @FXML private Button selectImageButton;
+    
+    @FXML private TextField planetName;
+    @FXML private TextField planetDiameterKM;
+    @FXML private TextField planetDiameterM;
+    @FXML private TextField planetMeanSurfaceTempC;
+    @FXML private TextField planetMeanSurfaceTempF;
+    @FXML private TextField planetNumberOfMoons;
+    
+    @FXML private Label fancyPlanetName;
     
     private Planet planet;
-    
+        
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1)
-	{
+	public void initialize( URL arg0, ResourceBundle arg1 ) {
 		planet = new Planet();
 		setPlanetImage("images/no_image.png");
 	}
 
     @FXML
-    void selectImage( ActionEvent event )
-    {
+    void selectImage( ActionEvent event ) {
     		FileChooser fileChooser = new FileChooser();
 		File file = fileChooser.showOpenDialog(new Stage());
+		
 		if (file == null)
 			return;
 
 		setPlanetImage(file.getAbsolutePath());
     }
     
-    private void setPlanetImage( String imagePath )
-    {
+    private void setPlanetImage( String imagePath ) {
     		planet.setPlanetImage(imagePath);
 		planetImage.setImage(planet.getPlanetImage());
 		planetImage.setId(planet.getPlanetImagePath());
     }
 
     @FXML
-    void loadPlanet( ActionEvent event )
-    {
-    	Alert alert = new Alert(AlertType.CONFIRMATION);
-    	alert.setTitle("OK to Overwrite?");
-    	alert.setContentText("Are you sure you want to overwrite the field values?");
-    	alert.showAndWait();
-    	
-    	if (alert.getResult() == ButtonType.CANCEL)
-    		return;
-    	
-    	FileChooser fileChooser = new FileChooser();
-    	fileChooser.getExtensionFilters().add(new ExtensionFilter("TXT files", "*.txt"));
-    	File file = fileChooser.showOpenDialog(new Stage());
-    	if (file == null)
-    		return;
-    	
-    	loadPlanetInformationFromFile(file);
-    }
-    
-    // delegate??
-    private void loadPlanetInformationFromFile( File file )
-    {
-    	try
-    	{
-    		Scanner scanner = new Scanner(file);
-    		setPlanetImage(scanner.nextLine());
-    		readPlanetName(scanner.nextLine());
-    		readPlanetDiameter(scanner.nextLine());
-    		readPlanetTemperature(scanner.nextLine());
-    		readPlanetNumberOfMoons(scanner.nextLine());
-    		scanner.close();
-    	}
-    	catch (FileNotFoundException e)
-    	{
-    		planet.showErrorAlert("Error: file not found\n" + e.getLocalizedMessage());
-    	}
-    	catch (NoSuchElementException e)
-    	{
-    		planet.showErrorAlert("Error: could not load planet information\n" + e.getLocalizedMessage());
-    	}
-    }
-    
-    // delegate??
-    private void readPlanetName( String name )
-    {
-    	planetName.setText(name);
-    	fancyPlanetName.setText(name);
-    }
-    
-    // delegate??
-    private void readPlanetDiameter( String diameterKM )
-    {
-    	if (diameterKM.equals(""))
-    		return;
-    	
-    	savePlanetDiameter(diameterKM);
-    }
-    
-    // delegate???
-    private void readPlanetTemperature( String temperatureC )
-    {
-    	if (temperatureC.equals(""))
-    		return;
-    	
-    	savePlanetMeanSurfaceTemp(temperatureC);
-    }
-    
-    private void readPlanetNumberOfMoons( String numberOfMoons )
-    {
-    	if (numberOfMoons.equals(""))
-    		return;
-    	
-    	savePlanetNumberOfMoons(numberOfMoons);
-    }
-    
-    @FXML
-    void savePlanet( ActionEvent event )
-    {
-    	if (!isPlanetReadyToSave())
-    		return;
-    	
-    	savePlanetToFile();
-    }
-    
-    // delegate??
-    private boolean isPlanetReadyToSave( )
-    {
-    	boolean returnCode = false;
-    	String imageId = planetImage.getId();
-    	
-    	if (imageId.compareTo("images/no_image.png") == 0)
-    		planet.showErrorAlert("Error: cannot save file\nNo image selected");
-    	else if (isEmptyString(planetName.getText()))
-    		planet.showErrorAlert("Error: cannot save file\nNo name specified");
-    	else if (isEmptyString(planetDiameterKM.getText()))
-    		planet.showErrorAlert("Error: cannot save file\nNo diameter specified");
-    	else if (isEmptyString(planetMeanSurfaceTempC.getText()))
-    		planet.showErrorAlert("Error: cannot save file\nNo temperature specified");
-    	else if (isEmptyString(planetNumberOfMoons.getText()))
-    		planet.showErrorAlert("Error: cannot save file\nNo number of moons specified");
-    	else
-    		returnCode = true;
-
-    	return returnCode;
-    }
-    
-    // delegate??
-    private boolean isEmptyString( String stringToCheck )
-    {
-    	if (stringToCheck.isEmpty())
-    		return true;
-    	
-    	return false;
-    }
-    
-    // delegate??
-    private void savePlanetToFile( )
-    {
-    	FileChooser fileChooser = new FileChooser();
-    	fileChooser.getExtensionFilters().add(new ExtensionFilter("TXT files", "*.txt"));
-    	
-    	File file = fileChooser.showSaveDialog(new Stage());
-    	
-    	if (file == null)
-    		return;
-    	
-    	writePlanetInformationToFile(file, getPlanetInformationString());
-    }
-    
-    // delegate??
-    private void writePlanetInformationToFile( File file, String fileContent )
-    {
-    	try
-    	{
-    		FileWriter fileWriter = new FileWriter(file);
-    		fileWriter.write(fileContent);
-    		fileWriter.close();
-    	}
-    	catch (IOException e)
-    	{
-    		planet.showErrorAlert("Error: Did not write Planet to file\n" + e.getLocalizedMessage());
-    	}
-    }
-    
-    private String getPlanetInformationString( )
-    {
-    	String planetInformation = "";
-    	
-    	planetInformation += planetImage.getId() + "\n";
-    	planetInformation += planetName.getText() + "\n";
-    	planetInformation += planetDiameterKM.getText() + "\n";
-    	planetInformation += planetMeanSurfaceTempC.getText() + "\n";
-    	planetInformation += planetNumberOfMoons.getText() + "\n";
-    	
-    	return planetInformation;
-    }
-    
-    @FXML
-    void setPlanetName( ActionEvent event )
-    {
-	    	String nameOfPlanet = planetName.getText();
-	
-	    	if (nameOfPlanet.length() < 0 || nameOfPlanet.length() > 256)
-	    		planet.showErrorAlert("Error: Planet name must be between 1 and 256 characters long");
-	    	else if (nameOfPlanet.matches("^[A-Za-z0-9 \\-\\.]+$"))
-	    	{
-	    		fancyPlanetName.setText(nameOfPlanet);
-	    		return;
-	    	}
-	    	else
-	    		planet.showErrorAlert("Error: Planet name contains invalid characters [A-Za-z0-9 -.]");
+    void loadPlanet( ActionEvent event ) {
+	    	Alert alert = new Alert(AlertType.CONFIRMATION);
+	    	alert.setTitle("OK to Overwrite?");
+	    	alert.setContentText("Are you sure you want to overwrite the field values?");
+	    	alert.showAndWait();
 	    	
-	    	planetName.setText("");
+	    	if (alert.getResult() == ButtonType.CANCEL)
+	    		return;
+	    	
+	    	FileChooser fileChooser = new FileChooser();
+	    	fileChooser.getExtensionFilters().add(new ExtensionFilter("TXT files", "*.txt"));
+	    	File file = fileChooser.showOpenDialog(new Stage());
+	    	if (file == null)
+	    		return;
+	    	
+	    	loadPlanetInformationFromFile(file);
+//	    	new PlanetBuilder(file);
+    }
+    
+    public void loadPlanetInformationFromFile( File file ) {
+	    	try 	{
+	    		readFromFile(file);
+	    	}
+	    	catch (FileNotFoundException e) {
+	    		planet.showErrorAlert("Error: file not found\n" + e.getLocalizedMessage());
+	    	}
+	    	catch (NoSuchElementException e) {
+	    		planet.showErrorAlert("Error: could not load planet information\n" + e.getLocalizedMessage());
+	    	}
+    }
+    
+    private void readFromFile( File file ) throws FileNotFoundException {
+		Scanner scanner = new Scanner(file);
+		setPlanetImage(scanner.nextLine());
+		readPlanetName(scanner.nextLine());
+		readPlanetDiameter(scanner.nextLine());
+		readPlanetTemperature(scanner.nextLine());
+		readPlanetNumberOfMoons(scanner.nextLine());
+		scanner.close();
+    }
+    
+    private void readPlanetName( String name ) {
+    		planet.savePlanetName(name);
+    		planetName.setText(planet.getPlanetName());
+	    	fancyPlanetName.setText(planet.getPlanetName());
+    }
+    
+    private void readPlanetDiameter( String diameterKM ) {
+	    	if (diameterKM.equals(""))
+	    		return;
+    		diameterKM = diameterKM.replaceAll("[.,]", "");
+	    	planet.savePlanetDiameter(diameterKM);    	
+	    	planetDiameterM.setText( planet.getPlanetDiameterM() );
+	    	planetDiameterKM.setText( planet.getPlanetDiameterKM() );
+    }
+    
+    private void readPlanetTemperature( String temperatureC ) {
+	    	if (temperatureC.equals(""))
+	    		return;
+	    	
+	    	planet.savePlanetMeanSurfaceTemp(temperatureC);
+	    	planetMeanSurfaceTempC.setText(planet.getPlanetMeanSurfaceTempC());
+	    	planetMeanSurfaceTempF.setText(planet.getPlanetMeanSurfaceTempF());
+    }
+    
+    private void readPlanetNumberOfMoons( String numberOfMoons ) {
+	    	if (numberOfMoons.equals(""))
+	    		return;
+	    	
+	    	planet.savePlanetNumberOfMoons(numberOfMoons);
+	    	planetNumberOfMoons.setText(planet.getPlanetNumberOfMoons());
     }
     
     @FXML
-    void setPlanetDiameter( ActionEvent event )
-    {
-    	savePlanetDiameter(planetDiameterKM.getText());
-    }
-    
-    // delegate???
-    private void savePlanetDiameter( String planetDiameterKMText )
-    {
-    	try
-    	{
-    		double diameterKM = Double.parseDouble(planetDiameterKMText);
-    		
-    		if (diameterKM < 0 || diameterKM > 200000)
-    			throw new NumberFormatException("Diameter (KM) must be between 0 and 200,000");
-    		
-    		String km = String.format("%,.1f", diameterKM);
-    		String mi = String.format("%,.1f", diameterKM * 0.621371);
-    		
-    		planetDiameterKM.setText(km);
-    		planetDiameterM.setText(mi);
-    	}
-    	catch (NumberFormatException e)
-    	{
-    		planetDiameterKM.setText("");
-    		planetDiameterM.setText("");
-    		planet.showErrorAlert("Error: Invalid diameter (KM) input\n" + e.getLocalizedMessage());
-    	}
+    void setPlanetName( ActionEvent event ) {
+	    	readPlanetName(planetName.getText());
     }
     
     @FXML
-    void setPlanetMeanSurfaceTemp( ActionEvent event )
-    {
-    	savePlanetMeanSurfaceTemp(planetMeanSurfaceTempC.getText());
-    }
-    
-    // delegate??
-    private void savePlanetMeanSurfaceTemp( String planetSurfaceTempCText )
-    {
-    	try 
-    	{
-    		double meanSurfaceTempC = Double.parseDouble(planetSurfaceTempCText);
-    		
-    		if (meanSurfaceTempC < -273.15 || meanSurfaceTempC > 500.0)
-    			throw new NumberFormatException("Degrees (C) must be between -273.15 and 500.00");
-    		
-    		planetMeanSurfaceTempC.setText("" + meanSurfaceTempC);
-    		planetMeanSurfaceTempF.setText("" + ((meanSurfaceTempC * 9 / 5) + 32));
-    	}
-    	catch (NumberFormatException e)
-    	{
-    		planetMeanSurfaceTempC.setText("");
-    		planetMeanSurfaceTempF.setText("");
-    		planet.showErrorAlert("Error: Invalid temperature (C) input\n" + e.getLocalizedMessage());
-    	}
+    void setPlanetDiameter( ActionEvent event ) {
+   		readPlanetDiameter(planetDiameterKM.getText());
     }
     
     @FXML
-    void setNumberOfMoons( ActionEvent event )
-    {
-    	savePlanetNumberOfMoons(planetNumberOfMoons.getText());
+    void setPlanetMeanSurfaceTemp( ActionEvent event ) {
+   		readPlanetTemperature(planetMeanSurfaceTempC.getText());
     }
     
-    // delegate??
-    private void savePlanetNumberOfMoons( String numberOfMoonsText )
-    {
-    	try 
-    	{
-    		int numberOfMoons = Integer.parseInt(numberOfMoonsText);
-    		
-    		if (numberOfMoons < 0 || numberOfMoons > 1000)
-    			throw new NumberFormatException("Number of moons must be between 0 and 1,000");
-    		
-    		String formattedNumberOfMoons = String.format("%,d", numberOfMoons);
-    		planetNumberOfMoons.setText(formattedNumberOfMoons);
-    	}
-    	catch (NumberFormatException e)
-    	{
-    		planetNumberOfMoons.setText("");
-    		planet.showErrorAlert("Error: Invalid Integer input\n" + e.getLocalizedMessage());
-    	}
+    @FXML
+    void setNumberOfMoons( ActionEvent event ) {
+    		readPlanetNumberOfMoons(planetNumberOfMoons.getText());
+    }
+    
+    @FXML
+    void savePlanet( ActionEvent event ) {
+	    	new PlanetBuilder(planetImage, planetName, planetDiameterKM, planetMeanSurfaceTempC, planetNumberOfMoons);    	
     }
 }
